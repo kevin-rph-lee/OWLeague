79da972c70ab71ca2ad5647f92ab05dfbb6314ae
Kevin Lee <kevin.rph.lee@gmail.com>
2018-12-23 14:32:40 -0800

    attempting with fresh heroku build

--- server.js
+++ server.js
@@ -148,7 +148,7 @@ app.post('/test/login', function(req, res) {
 
 
 console.log(secret)
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/client/build/index.html'))
 })
 
