5af95976d86435464a8ef148f375b8f361a83f4f
Kevin Lee <kevin.rph.lee@gmail.com>
2018-12-22 23:03:23 -0800

    removing loader after data is loading from bnet servers

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -14,7 +14,7 @@ import Loader from './Loader.js';
 class Home extends Component {
   constructor(props) {
     super(props);
-    this.state = { activeIndex: 0, items: [] };
+    this.state = { activeIndex: 0, items: [], loading: true };
     this.next = this.next.bind(this);
     this.previous = this.previous.bind(this);
     this.goToIndex = this.goToIndex.bind(this);
@@ -30,8 +30,7 @@ class Home extends Component {
     })
     .then((response) => {
       const data = response.data.data
-      console.log('Got Data')
-      console.log("Response Data: ", response.data)
+      this.setState({loading:false})
       for(var i in data){
 
         const items = this.state.items
@@ -78,6 +77,9 @@ class Home extends Component {
   }
 
   render() {
+
+    let content = null;
+
     const { activeIndex } = this.state;
 
     const slides = this.state.items.map((item) => {
@@ -93,20 +95,29 @@ class Home extends Component {
       );
     });
 
+    if(this.state.loading === true){
+      content = <Loader />
+    } else {
+      content =
+          <div className = "stages-carosel">
+            <Carousel
+              activeIndex={activeIndex}
+              next={this.next}
+              previous={this.previous}
+              // interval={0}
+            >
+              <CarouselIndicators items={this.state.items} activeIndex={activeIndex} onClickHandler={this.goToIndex} />
+              {slides}
+              <CarouselControl direction="prev" directionText="Previous" onClickHandler={this.previous} />
+              <CarouselControl direction="next" directionText="Next" onClickHandler={this.next} />
+            </Carousel>
+          </div>
+    }
+
+
     return (
-      <div className = "stages-carosel">
-        <Carousel
-          activeIndex={activeIndex}
-          next={this.next}
-          previous={this.previous}
-          // interval={0}
-        >
-          <CarouselIndicators items={this.state.items} activeIndex={activeIndex} onClickHandler={this.goToIndex} />
-          {slides}
-          <CarouselControl direction="prev" directionText="Previous" onClickHandler={this.previous} />
-          <CarouselControl direction="next" directionText="Next" onClickHandler={this.next} />
-        </Carousel>
-        <Loader />
+      <div>
+        {content}
       </div>
     );
   }
