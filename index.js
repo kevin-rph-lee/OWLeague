895a7b317a772bc4accc9d1faaedc37d569e59dd
Kevin <kevin.rph.lee@gmail.com>
2019-01-12 19:47:46 -0800

    moved teams route from index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const OWL = new OverwatchLeague();
 
 // Seperated Routes for each Resource
 const usersRoutes = require('./routes/users');
-// const teamsRoutes = require('./routes/teams');
+const teamsRoutes = require('./routes/teams');
 
 // Serve static files from the React frontend app
 app.use(express.static(path.join(__dirname, 'client/build')))
@@ -32,22 +32,10 @@ app.use(express.static(path.join(__dirname, 'client/build')))
 
 // Mount all resource routes
 app.use('/users', usersRoutes(knex, bcrypt));
-// app.use('/teams', teamsRoutes(knex));
+app.use('/teams', teamsRoutes(knex));
 
 
 
-
-app.get('/teams', function(req, res) {
-  console.log('Teams route')
-    knex
-      .select('*')
-      .from('teams')
-      .then((results) => {
-        console.log(results)
-        res.send(results);
-        // res.sendStats(300)
-      });
-});
 
 app.get('/owl', function(req, res) {
   let data = {};
--- routes/teams.js
+++ routes/teams.js
@@ -5,14 +5,14 @@ const router  = express.Router();
 
 module.exports = (knex) => {
 
-  // router.get('/', (req, res) => {
-  //   knex
-  //     .select('*')
-  //     .from('teams')
-  //     .then((results) => {
-  //       res.send(results);
-  //       // res.sendStats(300)
-  //     });
-  // });
+  router.get('/', (req, res) => {
+    knex
+      .select('*')
+      .from('teams')
+      .then((results) => {
+        res.send(results);
+        // res.sendStats(300)
+      });
+  });
   return router;
 }
